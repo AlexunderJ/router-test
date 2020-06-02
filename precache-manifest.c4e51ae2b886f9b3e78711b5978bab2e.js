2bed6f8de34426758217b33680a426be7c54adbe
AlexunderJ <pozdrower@wp.pl>
2020-06-02 10:28:51 +0200

    Updates

---
+++ precache-manifest.c4e51ae2b886f9b3e78711b5978bab2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fada4407ea8abc87939c400508df948",
+    "url": "/router-test/index.html"
+  },
+  {
+    "revision": "d390ecc74cd31f489353",
+    "url": "/router-test/static/css/main.d3c860dd.chunk.css"
+  },
+  {
+    "revision": "9ac52a6cfb52db9d7dbe",
+    "url": "/router-test/static/js/2.df13caf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/router-test/static/js/2.df13caf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d390ecc74cd31f489353",
+    "url": "/router-test/static/js/main.402d56c7.chunk.js"
+  },
+  {
+    "revision": "c17ce106a143ce6a3675",
+    "url": "/router-test/static/js/runtime-main.46e3abc9.js"
+  },
+  {
+    "revision": "c78027f6b151ca686078bcf2adf17c07",
+    "url": "/router-test/static/media/pike1.c78027f6.png"
+  },
+  {
+    "revision": "0db22f04ec4d3eaa9fbdd7f6c2f32d48",
+    "url": "/router-test/static/media/pike2.0db22f04.jpg"
+  },
+  {
+    "revision": "03c441a56f19c5d05c3f137baf2aa3e1",
+    "url": "/router-test/static/media/sail1.03c441a5.jpg"
+  }
+]);
